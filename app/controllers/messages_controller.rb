3ac431da08a2146a167fb65811f4916d310cca9b
tlockAOB <tlock@armyofbees.com>
2017-02-26 08:35:22 +0700

    polish inbox and friends list, and add page for new message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,8 @@ class MessagesController < ApplicationController
   def index
     @messages = current_user.received_messages.order("created_at DESC")
   end
+
+  def create
+    @message = Message.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
   resources :users
   root 'home#index'
-
   get 'users' => 'user#index'
+  get 'messages/new' => 'message#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
