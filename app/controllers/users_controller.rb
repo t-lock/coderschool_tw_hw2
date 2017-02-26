06123c67256fa88c593d8b1a4010d4f97307d052
tlockAOB <tlock@armyofbees.com>
2017-02-26 18:10:37 +0700

    tweak improper redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = "Congrats partner. You're in business..."
-      redirect_to root_path
+      redirect_to messages_path
     else
       render 'new'
     end
